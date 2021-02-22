f0b5f48e2a4f08c783090ac3c86c10db66533301
shahjalalcse <shah-jalal1>
2021-02-22 23:59:57 +0600

    dyanamic data pass

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+  const nayoks = ['anwar', 'apurbo', 'alomgir', 'salman shah']
   return (
     <div className="App">
       <header className="App-header">
        <p>I am a React</p>
-       <Person></Person>
-       <Person></Person>
-       <Person></Person>
+       <Person name={nayoks[0]}></Person>
+       <Person name="Mousumi"></Person>
+       <Person name="bapparaz"></Person>
+       <Person name="Arefin Nisho"></Person>
       </header>
     </div>
   );
 }
-function Person(){
+function Person(props){
+  console.log(props);
   return (
     // Multiple compoonent
   <div style={{
       border: '2px solid red', 
       margin: '10px'
     }}>
-    <h1>Name: Sakib Al Hasan</h1>
+    {/* <h1>Name: Sakib Al Hasan</h1> */}
+    <h1>Name: {props.name}</h1>
     <h3>Hero of the year</h3>
     </div>
   )
